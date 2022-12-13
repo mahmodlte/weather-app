18cd47702b78f03f4d7c0caff7fb2aa673968c7d
mahmodlte <mahmodlte@gmail.com>
2022-12-13 20:54:13 +0300

    Added Search bar to search for city in the navbar

---
+++ src/components/SearchCity.js
@@ -0,0 +1,13 @@
+const SearchCity = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        id="city_search"
+        placeholder="Enter City Name"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholderbg-gray-400 dark:text-white dark:focus:ring-blue-500  dark:focus:border-blue-500"
+      />
+    </div>
+  );
+};
+export default SearchCity;
