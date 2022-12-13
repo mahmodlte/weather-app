b4f911e71f304cba7ddb884820ff5f27dcfa3294
mahmodlte <mahmodlte@gmail.com>
2022-12-13 20:53:44 +0300

    Built Navbar of the app

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import SearchCity from "./SearchCity";
+import WeatherLogo from "../assets/Applogo.svg";
+const Header = () => {
+  return (
+    <div className=" flex items-center bg-gray-900">
+      <div className=" flex justify-center items-center my-2 mx-5">
+        <h1 className=" text-xl sm:text-2xl text-white ">Weatherly</h1>
+        <img
+          className="text-white"
+          width={50}
+          height={50}
+          src={WeatherLogo}
+          alt="Weather app logo"
+        />
+      </div>
+      <div className=" mx-16">
+        <SearchCity />
+      </div>
+    </div>
+  );
+};
+export default Header;
