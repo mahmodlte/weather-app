edef8721aa2d07043b0ea655bd9e03a8bec9f3cd
mahmodlte <mahmodlte@gmail.com>
2022-12-16 20:13:12 +0300

    Weather component call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Weather from "./containers/Weather";
 
 function App() {
   return (
     <div className=" ">
       <Header />
+      <Weather />
     </div>
   );
 }
