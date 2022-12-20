bf559024bae92754fa67fe4c84cf646404bc9565
mahmodlte <mahmodlte@gmail.com>
2022-12-20 11:45:21 +0300

    Added search query and auto complete endpoint

--- src/utils/weatherData.js
+++ src/utils/weatherData.js
@@ -1,9 +1,15 @@
 import { API_KEY, BASE_URL } from "./constants";
 
-export const getWeatherData = async (type) => {
+export const getWeatherData = async (type, query) => {
   const res = await fetch(
-    `${BASE_URL}/${type}.json?key=${API_KEY}&q=Erbil,IQ&aqi=yes`
+    `${BASE_URL}/${type}.json?key=${API_KEY}&q=${query}&aqi=yes`
   );
   const weatherData = res.json();
   return weatherData;
+};
+
+export const searchAutoComplete = async (query) => {
+  const res = fetch(`${BASE_URL}/v1/search.json?key=${API_KEY}&q=${query}`);
+  const searchResult = await res.json();
+  return searchResult;
 };
