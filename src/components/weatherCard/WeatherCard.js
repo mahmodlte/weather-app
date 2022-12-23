42eb530a65a95a71b6ab1db2e8d6fc6abec69762
mahmodlte <mahmodlte@gmail.com>
2022-12-23 20:15:23 +0300

    Style changes to work with the chart and moved the component to a weather card component

--- src/components/weatherCard/WeatherCard.js
+++ src/components/weatherCard/WeatherCard.js
@@ -1,3 +1,5 @@
+import HourlyForecastChart from "./HourlyForecastChart";
+
 const WeatherCard = ({ weather }) => {
   const currentTemp = (weather && weather.current.temp_c) || "";
   const feelsLike = (weather && weather.current.feelslike_c) || "";
@@ -14,14 +16,30 @@ const WeatherCard = ({ weather }) => {
     (weather && weather.current.air_quality["us-epa-index"]) || "";
 
   return (
-    <div className="max-w-sm mx-auto bg-white border-gray-200  rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <div className="flex flex-col md:flex-row justify-evenly dark:text-white ">
-        <section className="flex justify-between mx-2 my-2 ">
-          <div className="flex flex-col">
-            <p className="dark:text-white text-3xl"> {currentTemp}°C</p>
+    <div className="max-w-sm md:max-w-7xl mx-10 flex flex-col sm:justify-between sm:flex-row bg-white border-gray-200  rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex flex-col  dark:text-white ">
+        <section className="flex justify-between md:justify-start md:items-center mx-2 my-2 ">
+          <div className="flex md:mx-5 flex-col">
+            <p className="dark:text-white text-3xl "> {currentTemp}°C</p>
             <p>It feels like: {feelsLike}</p>
 
-            <p>
+            <p
+              className={
+                airQuality === 1
+                  ? "shadow-lg shadow-green-500 rounded-md"
+                  : airQuality === 2
+                  ? "shadow-lg shadow-amber-300 rounded-md"
+                  : airQuality === 3
+                  ? "shadow-lg shadow-orange-500 rounded-md"
+                  : airQuality === 4
+                  ? "shadow-lg shadow-orange-600 rounded-md"
+                  : airQuality === 5
+                  ? "shadow-lg shadow-orange-700 rounded-md"
+                  : airQuality === 6
+                  ? "shadow-lg shadow-red-600 rounded-md"
+                  : ""
+              }
+            >
               Air Quality:{" "}
               {airQuality === 1
                 ? "Good"
@@ -39,18 +57,18 @@ const WeatherCard = ({ weather }) => {
             </p>
           </div>
 
-          <div className="flex flex-col items-center justify-center ">
+          <div className="flex flex-col items-center justify-center  ">
             <img
-              className="w-20 h-20"
+              className="w-20 h-20 "
               src={condition.icon}
               alt=" weather condition icon"
             />
-            <div className="w-24 overflow-hidden">{condition.text}</div>
+            <div className=" overflow-hidden">{condition.text}</div>
           </div>
         </section>
-        <span className=" h-0.5 w-80 mx-auto my-2 bg-white lg:w1/3"></span>
-        <section className="flex justify-between">
-          <div className="mx-2 text-lg ">
+        <span className=" h-0.5 w-72 mx-auto my-2 md:hidden bg-white lg:w1/3"></span>
+        <section className="flex justify-between mx-6">
+          <div className=" text-lg ">
             <p className="my-0.5">Last Updated : {lastUpdated}</p>
             <p className="my-0.5">Precipitation: {precipitation} mm</p>
             <p className="my-0.5">Humidity: {humidity} %</p>
@@ -61,6 +79,11 @@ const WeatherCard = ({ weather }) => {
             <p className="my-0.5">UV index: {uv}</p>
           </div>
         </section>
+      </div>
+      <div className="w-auto h-96 md:w-2/4 lg:w-4/5 sm:h-96">
+        <HourlyForecastChart
+          weatherForecast={weather && weather.forecast.forecastday}
+        />
       </div>
     </div>
   );
