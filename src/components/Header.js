a90d4be11e0ceab9caffd026e6cc508aa60f4dbc
mahmodlte <mahmodlte@gmail.com>
2022-12-23 20:11:23 +0300

    Style changes to the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import SearchCity from "./SearchCity";
 import WeatherLogo from "../assets/Applogo.svg";
 const Header = ({ setSearch }) => {
   return (
-    <div className=" flex items-center bg-gray-900 ">
+    <div className=" flex items-start bg-gray-900 ">
       <div className=" flex justify-center items-center my-2 mx-5">
-        <h1 className=" text-xl sm:text-2xl text-white ">Weatherly</h1>
+        <h1 className=" text-xl sm:text-2xl text-white  ">Weatherly</h1>
         <img
           className="text-white"
           width={50}
@@ -13,7 +13,7 @@ const Header = ({ setSearch }) => {
           alt="Weather app logo"
         />
       </div>
-      <div className=" mx-16">
+      <div className="my-2">
         <SearchCity setSearch={setSearch} />
       </div>
     </div>
