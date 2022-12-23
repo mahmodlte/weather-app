f5c0cd923da4fbfc9822b862b1d992a924312643
mahmodlte <mahmodlte@gmail.com>
2022-12-23 20:14:20 +0300

    removed current weather data request

--- src/utils/weatherData.js
+++ src/utils/weatherData.js
@@ -1,16 +1,16 @@
 import { API_KEY, BASE_URL } from "./constants";
 
-export const getWeatherData = async (type, query) => {
-  const res = await fetch(
-    `${BASE_URL}/${type}.json?key=${API_KEY}&q=${query}&aqi=yes`
-  );
-  const weatherData = res.json();
-  return weatherData;
-};
-
 export const searchAutoComplete = async (query) => {
   const res = await fetch(`${BASE_URL}/search.json?key=${API_KEY}&q=${query}`);
 
   const searchResult = await res.json();
   return searchResult;
+};
+
+export const getWeatherForecast = async (query) => {
+  const res = await fetch(
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${query}&days=3&aqi=yes&alerts=no`
+  );
+  const forecast = await res.json();
+  return forecast;
 };
