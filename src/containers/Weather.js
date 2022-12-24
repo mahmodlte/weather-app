864bae9f5d2bc2c59bf00f3b94117adf4b9a0d40
mahmodlte <mahmodlte@gmail.com>
2022-12-24 15:12:33 +0300

    Change in style

--- src/containers/Weather.js
+++ src/containers/Weather.js
@@ -13,14 +13,15 @@ const Weather = ({ searchQuery }) => {
   }, [searchQuery]);
   console.log(weatherForecast);
   return (
-    <div className="h-auto bg-gray-900">
+    <div className=" h-auto md:h-screen bg-gray-900">
       <div>
         <div>
           <h1 className="text-white text-2xl px-10 py-10">Today Forecast</h1>
           <WeatherCard weather={weatherForecast} />
         </div>
-        <div>
-          <h1 className="text-white text-2xl px-10 py-10">2 Days Forecast</h1>
+        <h1 className="text-white text-2xl px-10 py-10">3 Days Forecast</h1>
+
+        <div className="sm:flex sm:mb-10">
           {weatherForecast &&
             weatherForecast.forecast.forecastday.map((day) => {
               return <DailyForecastCard weather={day} />;
