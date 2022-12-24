5466a4e6a197d25ad92ffa2799ab9f8c42b7aded
mahmodlte <mahmodlte@gmail.com>
2022-12-24 20:27:22 +0300

    Changed hover background color for city selection

--- src/components/SearchCity.js
+++ src/components/SearchCity.js
@@ -35,7 +35,7 @@ const SearchCity = ({ setSearch }) => {
         searchSuggestions.map((suggestion) => {
           return (
             <div
-              className="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-sm hover:bg-gray-900 hover:cursor-pointer focus:ring-blue-500 focus:border-blue-500 block w-full md:w-96 dark:bg-gray-700 dark:border-gray-600 dark:placeholderbg-gray-400 dark:text-white dark:focus:ring-blue-500  dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm hover:text-white hover:bg-gray-900 hover:cursor-pointer focus:ring-blue-500 focus:border-blue-500 block w-full md:w-96 dark:bg-gray-700 dark:border-gray-600 dark:placeholderbg-gray-400 dark:text-white dark:focus:ring-blue-500  dark:focus:border-blue-500"
               key={suggestion.id}
               onClick={() => onSuggestionHandler(suggestion.name)}
             >
