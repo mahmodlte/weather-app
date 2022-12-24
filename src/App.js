fd7052c986e4cdee364c400aecdc02dc306ab516
mahmodlte <mahmodlte@gmail.com>
2022-12-24 20:24:30 +0300

    Changed inital state of search

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Weather from "./containers/Weather";
 import { useState } from "react";
 function App() {
-  const [search, setSearch] = useState("Erbil,IQ");
+  const [search, setSearch] = useState();
 
   return (
     <div>
