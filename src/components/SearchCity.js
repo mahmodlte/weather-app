32c96722ce1c96769e9696a12f9bd7a518dcfcec
mahmodlte <mahmodlte@gmail.com>
2022-12-24 21:07:39 +0300

    Changed input type

--- src/components/SearchCity.js
+++ src/components/SearchCity.js
@@ -25,7 +25,7 @@ const SearchCity = ({ setSearch }) => {
       <input
         onChange={onQueryChange}
         required
-        type="query"
+        type="text"
         value={query}
         placeholder="Enter City Name"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-44 md:w-96  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholderbg-gray-400 dark:text-white dark:focus:ring-blue-500  dark:focus:border-blue-500"
